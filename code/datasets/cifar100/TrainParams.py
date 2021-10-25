c5bd754169f946032bc7d2c4d0a89f232374f260
aminbana <m.banayeean@gmail.com>
2021-10-25 14:00:34 +0330

    Final Config

--- code/datasets/cifar100/TrainParams.py
+++ code/datasets/cifar100/TrainParams.py
@@ -21,9 +21,9 @@ class MetaTrainParams():
     def __init__(self):
         super().__init__()
 
-        self.experiment_name = "cifar100_MAP"  # Name of experiment
+        self.experiment_name = "cifar100_Bayesian"  # Name of experiment
 
-        self.modelClass = MAP
+        self.modelClass = Bayesian
         self.backBone = ProtoNetBack
         self.temperature = 1
 
